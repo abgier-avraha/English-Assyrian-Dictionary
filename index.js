ca8ba0cfe5fe96115a5cf083257907937326573b
Abgier Avraha <abgier.avraha@gmail.com>
2018-12-22 16:56:57 +1100

    Well it certainly is possible now

--- index.js
+++ index.js
@@ -2,21 +2,190 @@ const jsreport = require("jsreport");
 const fs = require('fs');
 const outputFolder = 'output/';
 const outputFile = 'dictionary.pdf';
+const template = "templates/template.html.ejs";
+const wordsPerPage = 30;
 
+function chunk (arr, len) {
 
-fs.readFile("template.html", (err, data) => {
+  var chunks = [],
+      i = 0,
+      n = arr.length;
+
+  while (i < n) {
+    chunkLength = i === 0 ? len - 1 : len;
+    chunks.push(
+      {
+        heading: arr[0].english,
+        words: arr.slice(i, i += chunkLength),
+      }
+    );
+  }
+
+  return chunks;
+}
+
+fs.readFile(template, (err, data) => {
   createReport(data.toString());
 });
+
+const a = [
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'alpha', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'apple', syriac: 'ܐܣܛܪܢܓܠ'},
+{english: 'adam', syriac: 'ܐܣܛܪܢܓܠ'},
+]
 
 async function createReport(template) {
   try {
     const output = await jsreport.render({
+      data: {
+        wordGroups: [
+          {
+            letter: 'a'.toUpperCase(),
+            wordChunks: chunk(a, wordsPerPage),
+          },
+          {
+            letter: 'b'.toUpperCase(),
+            wordChunks: chunk(a, wordsPerPage),
+          },
+        ]
+      },
       template: {
-        content: template,
+        content: template.replace(/ }}/g, "}}").replace(/{{ /g, "{{"),
         recipe: "chrome-pdf",
-        engine: "handlebars",
+        engine: "ejs",
         chrome: {
-          headerTemplate: "<p>some header</p>",
           width: "840px",
           height: "1190px",
         }
